cbf3b30e1ae083d86ec233a66d9a4f82e946bf9d
Fabio D'Angelo <fabyoda@gmail.com>
2023-09-22 19:43:30 +0200

    skills bar fixed

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 
 // add class navbarDark on navbar scroll
 const header = document.querySelector(".navbar");
-
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add("navbarDark");
-    }
-    else {
-        header.classList.remove("navbarDark");
-    }
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top >= 100) {
+    header.classList.add("navbarDark");
+  }
+  else {
+    header.classList.remove("navbarDark");
+  }
 }
+
+
+
+// add skill animation after sroll on it
+function reveal() {
+  var reveals = document.querySelectorAll(".progress-bar");
+  for (var i = 0; i <reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+function reveal2() {
+  var reveals = document.querySelectorAll(".progress-bar-2");
+  for (var i = 0; i <reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal, reveal2);
